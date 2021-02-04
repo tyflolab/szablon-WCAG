b07b6f64c9005f87d29e3271abf67aa3499fd196
tyflolab <tyflolab@gmail.com>
2021-02-04 14:28:30 +0100

    Repaired generation of the table of contents structure

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,105 +1,115 @@
 /**
  * Controller handles all user interactions;
  */
-var ctrl = function() {
+var ctrl = (function () {
+  function navigateToPage() {
+    event.preventDefault();
+    const form = {};
+    $("#goToPageForm")
+      .serializeArray()
+      .forEach((element) => {
+        form[element.name] = element.value;
+      });
+    if (Util.isNotValidPageNumber(form.pageInput)) {
+      alert("Brak podanego numeru strony w publikacji");
+      return;
+    }
+    _navigateToPage(form.pageInput);
+  }
+  var Util = {
+    isNotValidPageNumber: function (pageNumber) {
+      return (
+        !Number.isInteger(+pageNumber) ||
+        +pageNumber > model.totalPageNumbers ||
+        +pageNumber < 1
+      );
+    },
+  };
 
-    function navigateToPage() {
-        event.preventDefault();
-        const form = {};
-        $("#goToPageForm").serializeArray().forEach(element => {
-            form[element.name] = element.value;
-        });
-        if (Util.isNotValidPageNumber(form.pageInput)) {
-            alert('Brak podanego numeru strony w publikacji');
-            return;
-        }
-        _navigateToPage(form.pageInput);
-    }
-    var Util = {
-        isNotValidPageNumber: function(pageNumber) {
-            return !Number.isInteger(+pageNumber) ||
-                +pageNumber > model.totalPageNumbers ||
-                +pageNumber < 1;
-        }
-    }
+  function navigateToNextPage() {
+    _navigateToPage(parseInt(model.currentPage) + 1);
+  }
 
-    function navigateToNextPage() {
-        _navigateToPage(parseInt(model.currentPage) + 1);
-    }
+  function navigateToPrevPage() {
+    _navigateToPage(parseInt(model.currentPage) - 1);
+  }
 
-    function navigateToPrevPage() {
-        _navigateToPage(parseInt(model.currentPage) - 1);
-    }
+  function navigateToNextChapter() {
+    _navigateToChapterWithOffset(1);
+  }
 
-    function navigateToNextChapter() {
-        _navigateToChapterWithOffset(1);
-    }
+  function navigateToPrevChapter() {
+    _navigateToChapterWithOffset(-1);
+  }
 
-    function navigateToPrevChapter() {
-        _navigateToChapterWithOffset(-1);
+  function _navigateToChapterWithOffset(offset) {
+    const currentChapterIndex = model.getChapterIndexById(
+      model.currentChapterId
+    );
+    if (currentChapterIndex < 0) {
+      console.error("Current chapter cannot be found in document structure");
+      return;
     }
+    const targetChapterNode =
+      model.documentStructure[currentChapterIndex + offset];
+    if (!targetChapterNode || targetChapterNode.name === "root") {
+      alert("Nie ma więcej rozdziałów");
+      return;
+    }
+    const targetElement = document.getElementById(targetChapterNode.id);
+    targetElement.scrollIntoView();
+    targetElement.focus();
+    model.currentChapterId = targetChapterNode.id;
+  }
 
-    function _navigateToChapterWithOffset(offset) {
-        const currentChapterIndex = model.getChapterIndexById(model.currentChapterId);
-        if (currentChapterIndex < 0) {
-            console.error('Current chapter cannot be found in document structure');
-            return;
-        }
-        const targetChapterNode = model.documentStructure[currentChapterIndex + offset]
-        if (!targetChapterNode || targetChapterNode.name === 'root') {
-            alert('Nie ma więcej rozdziałów')
-            return;
-        }
-        const targetElement = document.getElementById(targetChapterNode.id);
-        targetElement.scrollIntoView();
-        targetElement.focus();
-        model.currentChapterId = targetChapterNode.id;
+  function _navigateToPage(targetPageNumber) {
+    if (_isNotValidPageNumber(targetPageNumber)) {
+      alert("Nie ma więcej stron");
+      return;
     }
+    const targetElement = document.getElementById(
+      util.createPageId(targetPageNumber)
+    );
+    targetElement.scrollIntoView();
+    targetElement.focus();
+    model.currentPage = targetPageNumber;
+  }
 
-    function _navigateToPage(targetPageNumber) {
-        if (_isNotValidPageNumber(targetPageNumber)) {
-            alert('Nie ma więcej stron')
-            return;
-        }
-        const targetElement = document.getElementById(util.createPageId(targetPageNumber));
-        targetElement.scrollIntoView();
-        targetElement.focus();
-        model.currentPage = targetPageNumber;
-    }
+  function _isNotValidPageNumber(pageNumber) {
+    return (
+      !Number.isInteger(+pageNumber) ||
+      +pageNumber > model.totalPageNumbers ||
+      +pageNumber < 1
+    );
+  }
 
-    function _isNotValidPageNumber(pageNumber) {
-        return !Number.isInteger(+pageNumber) ||
-            +pageNumber > model.totalPageNumbers ||
-            +pageNumber < 1;
-    }
-
-    return {
-        navigateToPage,
-        navigateToNextPage,
-        navigateToPrevPage,
-        navigateToPrevChapter,
-        navigateToNextChapter
-    }
-}();
+  return {
+    navigateToPage,
+    navigateToNextPage,
+    navigateToPrevPage,
+    navigateToPrevChapter,
+    navigateToNextChapter
+  };
+})();
 
 var util = {
-    createPageId: function(pageNumber) {
-        return `page-${pageNumber}`;
-    }
-}
+  createPageId: function (pageNumber) {
+    return `page-${pageNumber}`;
+  },
+};
 
 /**
  * Main function
  */
-var main = function() {
-    view.createPages();
-    view.detectStructure();
-    view.createTableOfContent();
-    view.spyOnCurrentPage();
-    view.spyOnCurrentChapter();
-    view.createTooltips();
-    hljs.initHighlightingOnLoad();
-    view.createCopyButtons();
-    view.renderTree();
-    new ClipboardJS('.copy-btn');
-}();
+var main = (function () {
+  view.createPages();
+  view.detectStructure();
+  view.createTableOfContent();
+  view.spyOnCurrentPage();
+  view.spyOnCurrentChapter();
+  view.createTooltips();
+  hljs.initHighlightingOnLoad();
+  view.createCopyButtons();
+  view.renderTree();
+  new ClipboardJS(".copy-btn");
+})();
--- scripts/model.js
+++ scripts/model.js
@@ -1,37 +1,34 @@
 class DocumentNode {
-
-    constructor(id, level, name, parent, element) {
-        this.id = id;
-        this.level = level;
-        this.name = name;
-        this.parent = parent;
-        this.element = element;
-        this.children = [];
-    }
+  constructor(id, level, name, parent, element) {
+    this.id = id;
+    this.level = level;
+    this.name = name;
+    this.parent = parent;
+    this.element = element;
+    this.children = [];
+  }
 }
 
-var model = function () {
-    const documentRoot = new DocumentNode('', 1, 'root', null, null);
-    const totalPageNumbers = 0;
-    const currentPage = 1;
-    const currentChapterId = documentRoot.id;
-    const documentStructure = [documentRoot];
-    const documentPageElements = [];
+var model = (function () {
+  const documentRoot = new DocumentNode("", 1, "root", null, null);
+  const totalPageNumbers = 0;
+  const currentPage = 1;
+  const currentChapterId = documentRoot.id;
+  const documentStructure = [documentRoot];
+  const documentPageElements = [];
 
-    function getChapterIndexById(chapterId) {
-        return documentStructure.findIndex(node => node.id === chapterId);
-    }
+  function getChapterIndexById(chapterId) {
+    return documentStructure.findIndex((node) => node.id === chapterId);
+  }
 
-    return {
-        totalPageNumbers,
-        currentPage,
-        currentChapterId,
-        documentRoot,
-        documentStructure,
-        documentPageElements,
+  return {
+    totalPageNumbers,
+    currentPage,
+    currentChapterId,
+    documentRoot,
+    documentStructure,
+    documentPageElements,
 
-        getChapterIndexById
-    }
-}();
-
-
+    getChapterIndexById,
+  };
+})();
